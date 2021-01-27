68d4c5504dc1a08a6ae8ebbf125c1733e61bc45e
Yuliana-vish <68988546+Yuliana-vish@users.noreply.github.com>
2021-01-27 10:17:19 +0200

    Update theme.js

--- src/js/theme.js
+++ src/js/theme.js
@@ -5,9 +5,17 @@ const theme = {
   DARK: 'dark-theme',
 };
 
+switchTheme.addEventListener('change', checkboxState);
+
 function checkboxState(event) {
   localStorage.setItem('theme', JSON.stringify(event.target.checked));
   event.target.checked ? darkTheme() : lightTheme();
+}
+
+if (localStorage.getItem('theme')) {
+  const saveTheme = JSON.parse(localStorage.getItem('theme'));
+  switchTheme.checked = saveTheme;
+  switchTheme.checked ? darkTheme() : lightTheme();
 }
 
 function darkTheme() {
@@ -19,5 +27,3 @@ function lightTheme() {
   bodyRef.classList.add(theme.LIGHT);
   bodyRef.classList.remove(theme.DARK);
 }
-
-switchTheme.addEventListener('change', checkboxState);
